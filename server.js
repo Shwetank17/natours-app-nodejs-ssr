032e70532480065b55e34caec69adf5bf3441250
fs-projects <fs.projectsuk@gmail.com>
2020-06-30 19:10:47 +0530

    install mongoose and connect to database in atlas cloud

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE_REMOTE_VIA_APPLICATION_CONNECTION_STRING.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_REMOTE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Remote DB connection established...'))
+  .catch(error => console.log('There was error connecting to DB', error));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
-
-
-
-//app.get('env') - will give the value of the environment variable set by express in app.js 
+//app.get('env') - will give the value of the environment variable set by express in app.js
