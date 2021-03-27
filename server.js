31311266188b35a66e498463322eb8455bb5965a
fs-projects <fs.projectsuk@gmail.com>
2021-03-27 10:31:23 +0530

    Add: Mongoose schema and model and save to remote atlas db

--- server.js
+++ server.js
@@ -35,3 +35,69 @@ app.listen(port, () => {
 });
 
 //app.get('env') - will give the value of the environment variable set by express in app.js
+
+//Create a mongoose schema for TOURS
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    required: [true, 'A tour must have a rating']
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  },
+  numPersons: {
+    type: Number,
+    default: 5
+  }
+});
+
+//Create a model out of the created schema. The name of our model is 'Tour' so when .save is run on any of our document based out of 'Tour' model, like 'firstTour' here, then that document will be stored in 'tours' collection (plural of 'Tour') in natours-primary database in atlas hosted database
+const Tour = mongoose.model('Tour', tourSchema);
+const toursArray = [
+  {
+    name: 'The Bombay Troopers',
+    rating: 4.4,
+    price: '200',
+    numPersons: 5
+  },
+  {
+    name: 'The Kerala Rockers',
+    rating: 4.7,
+    price: '150',
+    numPersons: 3
+  },
+  {
+    name: 'The Delhi Warriors',
+    rating: 4.2,
+    price: '180',
+    numPersons: 3
+  },
+  {
+    name: 'The Pune Champions',
+    rating: 5,
+    price: '300',
+    numPersons: 5
+  },
+  {
+    name: 'The Bangalore Wind Chimes',
+    rating: 4,
+    price: '190',
+    numPersons: 4
+  }
+];
+
+toursArray.map(el => {
+  const documentToSave = new Tour(el);
+  documentToSave
+    .save()
+    .then(returnedDoc =>
+      console.log('Document saved successfully', returnedDoc)
+    )
+    .catch(error => console.log('Error saving the document', error));
+});
