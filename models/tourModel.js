9e68783853574eb3d2210fc08bcb6e1d144da1f3
fs-projects <fs.projectsuk@gmail.com>
2021-04-03 01:44:59 +0530

    Add: Document middlewares-pre and post hooks for save and create mongoose operations

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { default: slugify } = require('slugify');
 
 // Create a mongoose schema for TOURS. The Schema method also accepts second argument that we have used to configure our virtual property 'durationWeeks' to be shown in the resultant data returned from the query to mongodb. Virtuals are when you don't want to persist the property in the database. For example, say a user has a first and last name and you want to combine them to have a "fullName" property, but you don't want to store that in the database because it's redundant. In that scenario, you can create a virtual "fullName" property so you can access their full name in your code without having to concatenate the other two fields every time.
 const tourSchema = new mongoose.Schema(
@@ -55,17 +56,35 @@ const tourSchema = new mongoose.Schema(
       default: Date.now(),
       select: false
     },
-    startDates: [Date]
+    startDates: [Date],
+    slug: [String]
   },
   {
-    // 'toJSON' and 'toObject' means that if the result is JSON or object, virutal property must be returned. Also mongodb always returns an object on query.
+    // Configure virtual property. 'toJSON' and 'toObject' means that if the result is JSON or object, virutal property must be returned. Also mongodb always returns an object on it's query result.
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
   }
 );
 
+//Creating a virtual property that we can get on query to db. The value of this virtual property is defined inside of function
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
+});
+
+//Document Middleware(Hooks) : Runs before .save() and .create() mongoose methods, it will not work for insertMany, findOne, findByIdAndUpdate
+
+//Pre Hook
+tourSchema.pre('save', function(next) {
+  console.log('Document about to be saved is...', this);
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+//Post Hook
+tourSchema.post('save', function(doc, next) {
+  console.log('Document saved is...', this);
+  //Run your logic as in pre save above
+  next();
 });
 
 //Create a model out of the created schema. The name of our model is 'Tour' so when .save is run on any of our document based out of 'Tour' model, like 'firstTour' here, then that document will be stored in 'tours' collection (plural of 'Tour') in natours-primary database in atlas hosted database
