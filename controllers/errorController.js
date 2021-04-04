98d67253ea2f14054872ea77c8e5595448b5c9f5
fs-projects <fs.projectsuk@gmail.com>
2021-04-04 17:19:15 +0530

    Add: Including operationalErrors and sending differnt dev and prod error responses

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,34 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    stack: err.stack,
+    message: err.message.json
+  });
+};
+const sendErrorProd = (err, res) => {
+  // We know about the error so we trust it and send it to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message.json
+    });
+  } else {
+    // We don't know about the error so we don't send it to client instead log it for our own debugging
+    console.error(err);
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong...'
+    });
+  }
+};
 module.exports = (err, req, res, next) => {
   // console.log('stack trace is', err.stack);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  if (process.env === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env === 'production') {
+    sendErrorProd(err, res);
+  }
 };
